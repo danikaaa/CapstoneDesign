5925a9eeb4ce60e6681092521ea1949d521e8a2d
SUYEON LEE <82324322+danikaaa@users.noreply.github.com>
2022-05-15 12:25:04 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 $(function (){
     $('.instance_insert').click(function(){
         $('.instance_layer').fadeIn();
@@ -5,7 +6,15 @@ $(function (){
 
     $('.close').click(function (){
         $('.instance_layer').fadeOut();
+        $('.pw_layer').fadeOut();
     });
+
+    $('.pw_change').click(function(){
+        $('.pw_layer').fadeIn();
+    });
+
+
+
 
     $('.logOut_btn').click(function (){
 
@@ -32,6 +41,68 @@ $(function (){
 
     });
 
+    $('.cancel_btn').on('click', function(event){
+        $('.box_info').parent().find("input").val("");
+    });
+
+    $('.next_btn').on('click',function (event){
+       $('.menu_info').removeClass('on');
+       $('.menu_os').addClass('on');
+       $('.box_os').show();
+       $('.box_info').hide();
+       $('.cancel_btn').hide();
+       $('.next_btn').hide();
+
+       $('.back_btn_1').show();
+       $('.next_btn_1').show();
+
+    });
+
+    $('.back_btn_1').on('click', function (event){
+       $('.menu_info').addClass('on');
+       $('.menu_os').removeClass('on');
+
+        $('.box_info').show();
+        $('.box_os').hide();
+
+
+        $('.next_btn').show();
+        $('.cancel_btn').show();
+
+        $('.next_btn_1').hide();
+        $('.back_btn_1').hide();
+
+    });
+
+    $('.next_btn_1').on('click', function(event){
+       $('.menu_flavors').addClass('on');
+       $('.menu_os').removeClass('on');
+
+       $('.box_os').hide();
+       $('.box_flavors').show();
+
+       $('.next_btn_1').hide();
+       $('.create_btn').show();
+
+       $('.back_btn_1').hide();
+       $('.back_btn_2').show();
+
+    });
+
+    $('.back_btn_2').on('click', function(event){
+        $('.menu_flavors').removeClass('on');
+        $('.menu_os').addClass('on');
+
+        $('.box_os').show();
+        $('.box_flavors').hide();
+
+        $('.next_btn_1').show();
+        $('.create_btn').hide();
+
+        $('.back_btn_1').show();
+        $('.back_btn_2').hide();
+
+    });
 
 
 });
